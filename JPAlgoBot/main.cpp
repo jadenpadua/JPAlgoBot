744ba43d850b8ba7772bc300c8675fc6a093197e
Jaden Padua <paduajaden@gmail.com>
2023-01-16 21:40:34 -0800

    Initial Commit

---
+++ JPAlgoBot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JPAlgoBot
+//
+//  Created by Jaden Padua on 1/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

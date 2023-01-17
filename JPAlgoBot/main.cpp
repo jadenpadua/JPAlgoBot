381673c265b07b132acb7219ac5c5d4502de4b30
Jaden Padua <paduajaden@gmail.com>
2023-01-16 21:46:36 -0800

    Fix main msg

--- JPAlgoBot/main.cpp
+++ JPAlgoBot/main.cpp
@@ -8,7 +8,6 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "JPAlgoBot main\n";
     return 0;
 }
